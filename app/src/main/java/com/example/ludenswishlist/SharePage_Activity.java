becb3bb498e727b7067ba2896bfbbb174c5a4701
mw3357 <mw3357@tc.columbia.edu>
2020-04-22 23:20:00 -0400

    created share page - Manjun

---
+++ app/src/main/java/com/example/ludenswishlist/SharePage_Activity.java
@@ -0,0 +1,14 @@
+package com.example.ludenswishlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SharePage_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_page_);
+    }
+}
